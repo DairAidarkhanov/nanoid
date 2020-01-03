c09136fc5ef7bbf1308a2633c52e9c35cc7612a6
aidarkhanov <dairaidarkhanov@gmail.com>
2020-01-03 22:43:40 +0600

    Adjusted example to changes

--- nanoid_example_test.go
+++ nanoid_example_test.go
@@ -3,12 +3,11 @@ package nanoid_test
 import (
 	"crypto/rand"
 	"fmt"
-	"log"
 
 	"github.com/aidarkhanov/nanoid"
 )
 
-func ExampleFormatString() {
+func ExampleFormat() {
 	alphabet := "-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW"
 	size := 21
 
@@ -21,31 +20,55 @@ func ExampleFormatString() {
 		return buffer, nil
 	}
 
-	id, err := nanoid.FormatString(generateBytesBuffer, alphabet, size)
+	id, err := nanoid.Format(generateBytesBuffer, alphabet, size)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
 	fmt.Println(id)
 }
 
-func ExampleGenerateString() {
+func ExampleGenerate() {
 	alphabet := "-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW"
 	size := 21
 
-	id, err := nanoid.GenerateString(alphabet, size)
+	id, err := nanoid.Generate(alphabet, size)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
+
+	fmt.Println(id)
+}
+
+func ExampleMustFormat() {
+	alphabet := "-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW"
+	size := 21
+
+	// generateBytesBuffer returns random bytes buffer
+	generateBytesBuffer := func(step int) ([]byte, error) {
+		buffer := make([]byte, step)
+		if _, err := rand.Read(buffer); err != nil {
+			return nil, err
+		}
+		return buffer, nil
+	}
+
+	id := nanoid.MustFormat(generateBytesBuffer, alphabet, size)
+
+	fmt.Println(id)
+}
+
+func ExampleMustGenerate() {
+	alphabet := "-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW"
+	size := 21
+
+	id := nanoid.MustGenerate(alphabet, size)
 
 	fmt.Println(id)
 }
 
 func ExampleNew() {
-	id, err := nanoid.New()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	id := nanoid.New()
 
 	fmt.Println(id)
 }
